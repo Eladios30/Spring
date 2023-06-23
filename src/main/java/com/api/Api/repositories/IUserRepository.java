e4d47b203775018510e27110738d9893e998540b
Eladio Quintero <116222995+Eladios30@users.noreply.github.com>
2023-06-22 20:57:27 -0700

    Subete pero ya!11

---
+++ src/main/java/com/api/Api/repositories/IUserRepository.java
@@ -0,0 +1,10 @@
+package com.api.Api.repositories;
+
+import com.api.Api.models.UserModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IUserRepository extends JpaRepository<UserModel, Long> {
+
+}
